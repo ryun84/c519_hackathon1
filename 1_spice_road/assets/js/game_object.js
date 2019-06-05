e9f4a6062a8ed783ff870bb6c79101070727a7ef
Richard Yun <richardyun84@gmail.com>
2019-06-05 15:31:39 -0700

    edited the createGameStartCard function

--- 1_spice_road/assets/js/game_object.js
+++ 1_spice_road/assets/js/game_object.js
@@ -1,11 +1,12 @@
-
-
-class Game{
-    constructor(){
+class Game {
+    constructor() {
         this.currentPlayer = null; // will be either playerOne or playerTwo
         this.totalTurns = 0;
+        this.pointsCardArray = [];
+        this.merchantCardArray = [];
     }
 
+<<<<<<< HEAD
     initiateGame(){
         var startingYellowSpiceValue = null;
         // will need to add the starting yellow spice values to both players
@@ -16,10 +17,32 @@ class Game{
     }
 
     createGameStartCard(){
+=======
+    createGameStartCard() {
+>>>>>>> dca12d8ba17c8619e5a4c8ea52491bcb0b4a97b3
         var startingCard = $("<civ>").addClass("card").text("3 YELLOW");
         // the above creation is probably not the way we want to do this
         // this is just a static element that will start with 3 yellow spices and be placed into the availableCards(?) array of both players
         // but once this is created, it should still function like any other merchant card and be able to be popped/deleted from players availableCards(?) array and
         // pushed into the discardedCards(?) array (and vice versa after clicking REST), all while updating what is displayed.
+    }
+
+    createFirstPointCards() {
+        for (var i = 0; i < 5; i++) {
+            this.generatePointCards();
+        }
+    }
+
+    createFirstMerchantCards() {
+        for (var i = 0; i < 6; i++) {
+            MerchantCard.createMerchantCard();
+        }
+
+    }
+
+    generatePointCards() {
+        var pointsValue = Math.floor(Math.random() * 20) - 6;
+        this.pointsCardArray.push(pointsValue);
+
     }
 }
