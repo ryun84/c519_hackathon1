59cc42df39f7aad68511b326332225376b54cd8b
Richard Yun <richardyun84@gmail.com>
2019-06-05 14:55:38 -0700

    Made a initiateGame function to set the starting yellow spice value also made this function call the createGameStartCard function (which still needs editing)

--- 1_spice_road/assets/js/game_object.js
+++ 1_spice_road/assets/js/game_object.js
@@ -6,8 +6,20 @@ class Game{
         this.totalTurns = 0;
     }
 
+    initiateGame(){
+        var startingYellowSpiceValue = null;
+        // will need to add the starting yellow spice values to both players
+        $(".yellow").text(startingYellowSpiceValue);
+
+        // will need to call the createGameStartCard function
+        createGameStartCard();
+    }
+
     createGameStartCard(){
         var startingCard = $("<civ>").addClass("card").text("3 YELLOW");
+        // the above creation is probably not the way we want to do this
         // this is just a static element that will start with 3 yellow spices and be placed into the availableCards(?) array of both players
+        // but once this is created, it should still function like any other merchant card and be able to be popped/deleted from players availableCards(?) array and
+        // pushed into the discardedCards(?) array (and vice versa after clicking REST), all while updating what is displayed.
     }
 }
