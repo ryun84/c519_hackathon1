7e4994601a52ad4b31f54aa818b53bdd4577e6ff
Richard Yun <richardyun84@gmail.com>
2019-06-05 16:07:44 -0700

    trying to save and get most current version of dev

--- 1_spice_road/assets/js/game_object.js
+++ 1_spice_road/assets/js/game_object.js
@@ -1,14 +1,20 @@
 class Game {
     constructor() {
-        this.currentPlayer = null; // will be either playerOne or playerTwo
+        this.currentPlayer = null; // will be either "playerOne" or "playerTwo"
         this.totalTurns = 0;
         this.pointsCardArray = [];
         this.merchantCardArray = [];
     }
 
-    createGameStartCard() {
-        var startingCard = $("<civ>").addClass("card").text("3 YELLOW");
-        // this is just a static element that will start with 3 yellow spices and be placed into the availableCards(?) array of both players
+    createGameStartCard(){
+        var startingCard = {
+            // properties of however many yellow spices we want the players to start with
+        }
+        var startingCardDivIndex = $("*[data-index='0']");
+        // this is selecting the first div in the availableCards container div
+        // once the above div is selected, we will have to pass in value from startingCard
+        // this card should still function like any other merchant card and be able to be popped/deleted from players availableCards(?) array and...
+        // pushed into the discardedCards(?) array (and vice versa after clicking REST), all while updating what is displayed.
     }
 
     createFirstPointCards() {
