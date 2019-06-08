27b981e7dc79a6514beb46d3e718bcc2f3fbecec
Richard Yun <richardyun84@gmail.com>
2019-06-08 15:01:14 -0700

    reverted code pre-meeting

--- 1_spice_road/assets/js/game_object.js
+++ 1_spice_road/assets/js/game_object.js
@@ -1,36 +1,35 @@
 class Game {
     constructor() {
-        this.pointClickHandler = this.pointClickHandler.bind(this); //going to contain individual numbers alongside arrays with 2 values so we will need to search for arrays and display the values inside the array.
-        this.playerRetrieveMerchantCardAndAddToHand = this.playerRetrieveMerchantCardAndAddToHand.bind(this);
-        this.merchantClickHandler = this.merchantClickHandler.bind(this);
-        this.endTurn = this.endTurn.bind(this);
         this.playerOne = new Player();
         this.playerTwo = new Player();
         this.playerOne.yellow = 3;
         this.playerTwo.yellow = 4;
         this.currentPlayer = this.playerOne;
-        this.availablePointsCardsArrayforPlayerToClickAndClaim = [];
-        this.availableMerchCardsArrayforPlayertoClickAndC = [];
+        this.totalTurns = 0;
+        this.pointsCardArray = [];
+        this.merchantCardArray = [];
         this.playerOne.merchantCardsInHand.push(3);
-        this.playerTwo.merchantCardsInHand.push(3);
+        this.playerTwo.merchantCardsInHand.push(3)
+        this.pointClickHandler = this.pointClickHandler.bind(this); //going to contain individual numbers alongside arrays with 2 values so we will need to search for arrays and display the values inside the array.
+        this.endTurn = this.endTurn.bind(this);
         this.firstTurn = true;
         this.winCondition = 5;
-        this.isLastTurn = 0;
+        this.turnsLeft = 0;
+        this.getMerchantToHand = this.getMerchantToHand.bind(this);
+
+        this.merchantClickHandler = this.merchantClickHandler.bind(this);
+
         this.disableClick = false;
     }
 
-    applyClickHandlers() {
+    clickHandlers() {
         $('.points').click(this.pointClickHandler);
         $('.playerMerchant').click(this.merchantClickHandler);
-        $('.merchant').click(this.playerRetrieveMerchantCardAndAddToHand);
+        $('.merchant').click(this.getMerchantToHand);
         $('.discardArea').click(this.endTurn);
     }
 
-    generalRandomNumberGenerator( min, max ){
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-
-    playerRetrieveMerchantCardAndAddToHand(event) {
+    getMerchantToHand(event) {
         if (this.disableClick === true) {
             return;
         }
@@ -38,16 +37,16 @@ class Game {
             return;
         }
         var merchIndex = parseFloat($(event.currentTarget).attr('data-index'));
-        var merchCardInfo = this.availableMerchCardsArrayforPlayertoClickAndC[merchIndex];
+        var merchCardInfo = this.merchantCardArray[merchIndex];
         this.currentPlayer.merchantCardsInHand.push(merchCardInfo);
         var newMerchCard;
-        var rNG = generalRandomNumberGenerator(1,2);
+        var rNG = Math.ceil(Math.random() * 2);
         if (rNG === 1) {
             var newMerchCard = this.createMerchantGatherCard();
         } else {
             var newMerchCard = this.createMerchantTradeCard();
         }
-        this.availableMerchCardsArrayforPlayertoClickAndC.splice(merchIndex, 1, newMerchCard);
+        this.merchantCardArray.splice(merchIndex, 1, newMerchCard);
         this.displayMerchantCardsInHand();
         this.displayMerchantCardInfo();
         this.disableClick = true;
@@ -100,8 +99,8 @@ class Game {
     }
 
     generatePointCards() {
-        var pointsValue = generalRandomNumberGenerator(6,25);
-        this.availablePointsCardsArrayforPlayerToClickAndClaim.push(pointsValue);
+        var pointsValue = Math.floor(Math.random() * 20) + 6;
+        this.pointsCardArray.push(pointsValue);
     }
 
     pointClickHandler(event) {
@@ -111,14 +110,14 @@ class Game {
         var pointIndex = $(event.currentTarget).attr('data-index');
         if (this.currentPlayer.yellow > 0) {
             this.currentPlayer.yellow -= 1;
-            this.currentPlayer.victoryPoints += this.availablePointsCardsArrayforPlayerToClickAndClaim[pointIndex];
+            this.currentPlayer.victoryPoints += this.pointsCardArray[pointIndex];
             this.currentPlayer.pointCardCount += 1;
-            this.availablePointsCardsArrayforPlayerToClickAndClaim.splice( pointIndex, 1, generalRandomNumberGenerator(6,25) );
+            this.pointsCardArray.splice(pointIndex, 1, Math.floor(Math.random() * 20) + 6);
             this.updateVictoryPointsDisplay();
             this.updateVictoryPointCardsDisplay();
             this.updateSpiceCountDisplay();
             this.disableClick = true;
-            if (this.isLastTurn === 0) {
+            if (this.turnsLeft ===0){
                 setTimeout(this.endTurn, 3000);
             }
         } else {
@@ -139,75 +138,67 @@ class Game {
     }
 
     updateVictoryPointCardsDisplay() {
-        var pointsCards = $(".pointsCardRowDiv .card");
-        for (var i = 0; i < pointsCards.length; i++) {
-            pointsCards.eq(i) = this.availablePointsCardsArrayforPlayerToClickAndClaim[i];
-        }
-        // $("#zeroIndex").html("Victory Points<br/> " + this.availablePointsCardsArrayforPlayerToClickAndClaim[0]);
-        // $("#oneIndex").html("Victory Points<br/>" + this.availablePointsCardsArrayforPlayerToClickAndClaim[1]);
-        // $("#twoIndex").html("Victory Points<br/>" + this.availablePointsCardsArrayforPlayerToClickAndClaim[2]);
-        // $("#threeIndex").html("Victory Points<br/>" + this.availablePointsCardsArrayforPlayerToClickAndClaim[3]);
-        // $("#fourIndex").html("Victory Points<br/>" + this.availablePointsCardsArrayforPlayerToClickAndClaim[4]);
+        $("#zeroIndex").html("Victory Points<br/> " + this.pointsCardArray[0]);
+        $("#oneIndex").html("Victory Points<br/>" + this.pointsCardArray[1]);
+        $("#twoIndex").html("Victory Points<br/>" + this.pointsCardArray[2]);
+        $("#threeIndex").html("Victory Points<br/>" + this.pointsCardArray[3]);
+        $("#fourIndex").html("Victory Points<br/>" + this.pointsCardArray[4]);
     }
 
     createMerchantCard() {
-        var rNG = generalRandomNumberGenerator(1,2);
+        var rNG = Math.ceil(Math.random() * 2);
         if (rNG === 1) {
             var gatherCard = this.createMerchantGatherCard();
-            this.availableMerchCardsArrayforPlayertoClickAndC.push(gatherCard);
+            this.merchantCardArray.push(gatherCard);
         } else {
             var tradeCard = this.createMerchantTradeCard();
-            this.availableMerchCardsArrayforPlayertoClickAndC.push(tradeCard);
+            this.merchantCardArray.push(tradeCard);
         }
         this.displayMerchantCardInfo();
     }
 
     checkWinCondition() {
         if (this.playerOne.pointCardCount === 5 || this.playerTwo.pointCardCount === 5) {
-            if (this.isLastTurn === 1) {
+            if (this.turnsLeft === 1) {
                 if (this.playerOne.victoryPoints > this.playerTwo.victoryPoints) {
                     this.updateVictoryPointCardsDisplay();
                     alert("Player One wins with " + this.playerOne.victoryPoints + " vs Player Two with " + this.playerTwo.victoryPoints)
                     return;
                 } else {
-                    this.updateVictoryPointCardsDisplay();
+                    updateVictoryPointCardsDisplay();
                     alert("Player Two wins with " + this.playerTwo.victoryPoints + " vs Player One with " + this.playerOne.victoryPoints)
                     return;
                 }
             }
-            if (this.currentPlayer === this.playerOne) {
+            if (this.currentPlayer === this.playerOne) {              
                 alert("Player Two has one turn left to outspice Player One");
-                this.isLastTurn = 1;
+                this.turnsLeft = 1;
                 setTimeout(this.endTurn, 3000);
-            } else {
+            } else {                
                 alert("Player One has one turn left to outspice Player Two");
-                this.isLastTurn = 1;
+                this.turnsLeft = 1;
                 setTimeout(this.endTurn, 3000);
             }
         }
     }
 
     createMerchantGatherCard() {
-        var spiceValue = generalRandomNumberGenerator(2,5);
+        var spiceValue = Math.floor(Math.random() * 4) + 2;
         return spiceValue;
     }
 
     createMerchantTradeCard() {
-        var topValue = generalRandomNumberGenerator(2,6); // should change variables to be more readable
-        var bottomValue = generalRandomNumberGenerator(2,6);
+        var topValue = Math.floor(Math.random() * 5) + 2;
+        var bottomValue = Math.floor(Math.random() * 5) + 2;
         var tradeCard = [];
-        var tradeSpecifications = {
-            cost: topValue,
-            yeilds: bottomValue
-        }
-        tradeCard.push(tradeSpecifications);
-        // tradeCard.push(bottomValue);
+        tradeCard.push(topValue);
+        tradeCard.push(bottomValue);
         return tradeCard;
     }
 
     displayMerchantCardInfo() {
         for (var counter = 0; counter < 6; counter++) {
-            var merchantInnerArray = this.availableMerchCardsArrayforPlayertoClickAndC[counter];
+            var merchantInnerArray = this.merchantCardArray[counter];
             if (Array.isArray(merchantInnerArray)) {
                 $(".merchantCardRowDiv [data-index='" + counter + "']").html("Trade " + merchantInnerArray[0] + "<br/> for " + merchantInnerArray[1]);
             } else {
@@ -216,16 +207,7 @@ class Game {
         }
     }
 
-    checkIfCanAfford(cost, playerResources) {
-        for (var key in playerResources) {
-            if (cost[key] > playerResources[key]) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    merchantClickHandler(event) {
+    merchantClickHandler(event){
         if (this.disableClick === true) {
             return;
         }
@@ -234,24 +216,7 @@ class Game {
         if (Array.isArray(merchCardToUseData)) {
             var spiceToDeduct = merchCardToUseData[0];
             var spiceToAdd = merchCardToUseData[1];
-            this.currentPlayer.spices = {
-                yellow: 1,
-                red: 2,
-                green: 3,
-                brown: 0
-            }
-            var cost = {
-                yellow: 0,
-                red: 1,
-                green: 2,
-                brown: 1
-            }
-            if (!this.checkIfCanAfford(cost, this.currentPlayer.spices)) {
-                alert("You do not have enough resources!");
-            } else {
-                // deduct
-            }
-            if (spiceToDeduct > this.currentPlayer.yellow) {
+            if( spiceToDeduct > this.currentPlayer.yellow ){
                 alert("You do not have enough resources!");
             } else {
                 this.currentPlayer.yellow -= spiceToDeduct;
@@ -266,8 +231,8 @@ class Game {
         setTimeout(this.endTurn, 3000);
     }
 
-    updateSpiceCountDisplay() {
-        if (this.currentPlayer === this.playerOne) {
+    updateSpiceCountDisplay(){
+        if( this.currentPlayer === this.playerOne ){
             $("#p1_sq4").html("Yellow Spices<br/>" + this.playerOne.yellow);
         } else {
             $("#p2_sq4").html("Yellow Spices<br/>" + this.playerTwo.yellow);
